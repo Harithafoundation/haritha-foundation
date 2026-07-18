7d469b77b67bdab5ca4701adc5c5f02b41649b09
unknown <pasupulatipavan21@gmail.com>
2026-07-18 13:59:11 +0530

    email updation

--- haritha_foundation/settings.py
+++ haritha_foundation/settings.py
@@ -151,13 +151,14 @@ LOGIN_URL='/login/'
 RAZORPAY_KEY_ID="rzp_test_T685deq4zmzwCv"
 RAZORPAY_KEY_SECRET="pmG1oJdnBA7KIE7NfWybPHeI"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND,django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL,EMAIL_HOST_USER")
+EMAIL_TIMEOUT=30
 
 JAZZMIN_SETTINGS={
     'site_title':'Haritha Foundation Admin',
