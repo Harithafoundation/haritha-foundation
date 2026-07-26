f1cfd1006d3bf3df2c4bdca939b17e3ca0669080
unknown <pasupulatipavan21@gmail.com>
2026-07-26 18:59:52 +0530

    added robots.txt

--- haritha_foundation/urls.py
+++ haritha_foundation/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import StaticViewSitemap
+from django.views.generic import RedirectView
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -38,6 +39,10 @@ urlpatterns = [
     path('education/',include('education.urls')),
     path('core/',include('core.urls')),
     path('sitemap.xml',sitemap,{'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt",RedirectView.as_view(url="/static/robots.txt", permanent=True),),
 
 ]
+
 urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
